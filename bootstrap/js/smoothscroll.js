31810fa7940d4e1e0ac82f3ffaeb4bad0af5cb5f
frogue <zzmaric@gmail.com>
2014-03-15 03:18:22 +0100

    Implemented smooth scrolling and scrollspy. The mobile menu is now messed up.

---
+++ bootstrap/js/smoothscroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+     $('#nav ul li a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top - 49
+         }, 900, 'swing', function () {
+             // window.location.hash = target;
+         });
+     });
+ });
